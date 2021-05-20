9a5a779ad4afba290cf861250ccd6c724632fe42
TROYNELSON12 <troyjnelson12@gmail.com>
2021-05-20 14:57:50 -0400

    Added support for Discord Error Reporting

--- Assets/Scripts/System/ErrorManager.cs
+++ Assets/Scripts/System/ErrorManager.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Collections.Specialized;
+using System.Net;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Linq;
 
 public class ErrorManager : MonoBehaviour
 {
@@ -42,6 +44,9 @@ public class ErrorManager : MonoBehaviour
     {
         if (Type == LogType.Exception)
         {
+            DiscordErrorReport("Error recieved at " + DateTime.Now);
+            DiscordErrorReport(Log);
+            DiscordErrorReport(Trace);
             StopAllCoroutines();
             StartCoroutine(ErrorDisplay(Log + " " + Trace, Color.red));
         }
@@ -57,5 +62,48 @@ public class ErrorManager : MonoBehaviour
         yield return new WaitForSeconds(5);
         ErrorText.text = "Error";
         ErrorBox.SetActive(false);
+    }
+
+    void DiscordErrorReport(string Error)
+    {
+        using (DiscordWebHook dcWeb = new DiscordWebHook())
+        {
+            dcWeb.ProfilePicture = "http://alphawarestudios.com/Assets/Sprites/RoboScout-Logo.png";
+            dcWeb.UserName = "RoboScout Error Report";
+            dcWeb.WebHook = "https://discord.com/api/webhooks/782645093906776074/pTbpLXHqeovb_BDuSwcWohfX9CZSWn6J1ssC5S08UGsjeuWL4xkJcktc7SgVnacAc8JR";
+            string Output = Error;
+
+            dcWeb.SendMessage(Output);
+            dcWeb.Dispose();
+        }
+    }
+}
+
+public class DiscordWebHook : IDisposable
+{
+    private WebClient dWebClient;
+    private static NameValueCollection discordValues = new NameValueCollection();
+    public string WebHook { get; set; }
+    public string UserName { get; set; }
+    public string ProfilePicture { get; set; }
+
+    public DiscordWebHook()
+    {
+        dWebClient = new WebClient();
+    }
+
+    public void SendMessage(string msgSend)
+    {
+        discordValues.Clear();
+        discordValues.Add("username", UserName);
+        discordValues.Add("avatar_url", ProfilePicture);
+        discordValues.Add("content", msgSend);
+
+        dWebClient.UploadValues(WebHook, discordValues);
+    }
+
+    public void Dispose()
+    {
+        dWebClient.Dispose();
     }
 }
